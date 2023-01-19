80442fa7d949ce867179f6bfe05869fd7d6f28ae
sappkevin <109696421+sappkevin@users.noreply.github.com>
2023-01-19 11:19:09 -0800

    Resolved bug fixes with original code
    Fixed the indexes to
    
        name = row[2]
        position = row[0]
        salary = int(row[5])
        points = float(row[8])
    
    and 
    Doesnt account for F position so I removed it and changed the limit of SF position to 2 (since Draftkings doesn't use F as a position it uses PF and SF instead)

--- nba_lineup_knapsack.py
+++ nba_lineup_knapsack.py
@@ -2,7 +2,7 @@ import csv
 import argparse
 
 arg_p = argparse.ArgumentParser()
-arg_p.add_argument('-csv', default='')
+arg_p.add_argument('-csv', default='DKSalaries.csv')
 args = vars(arg_p.parse_args())
 
 CSV = args['csv']
@@ -27,10 +27,10 @@ with open(CSV,'r') as data:
     next(reader)
     players = []
     for row in reader:
-        name = row[1]
+        name = row[2]
         position = row[0]
-        salary = int(row[2])
-        points = float(row[4])
+        salary = int(row[5])
+        points = float(row[8])
         value = points / salary  
         player = Player(position, name, salary, points, value)
         players.append(player)
@@ -41,7 +41,7 @@ def lineup_knapsack(players):
     limits = {
         'PG':1,
         'SG':1,
-        'SF':1,
+        'SF':2,
         'PF':1,
         'C':1,
         'G':1,
@@ -80,17 +80,17 @@ def lineup_knapsack(players):
             continue
         if counter['SF'] < limits['SF'] and current_lineup_cap + sal <= cap and pos in ['SF','SF/PF']:
             lineup.append(player)
-            counter['SF'] = counter['SF'] + 1
+            counter['SF'] += 1
             current_lineup_cap += sal
             continue
         if counter['PF'] < limits['PF'] and current_lineup_cap + sal <= cap and pos in ['PF','PF/C']:
             lineup.append(player)
-            counter['PF'] = counter['PF'] + 1
+            counter['PF'] += 1
             current_lineup_cap += sal
             continue
-        if counter['C'] < limits['C'] and current_lineup_cap + sal <= cap and pos in ['C','PF/C']:
+        if counter['C'] < limits['C'] and current_lineup_cap + sal <= cap and pos in ['C']:
             lineup.append(player)
-            counter['C'] = counter['C'] + 1
+            counter['C'] += 1
             current_lineup_cap += sal
             continue
         if counter['G'] < limits['G'] and current_lineup_cap + sal <= cap and pos in ['PG','SG']:
@@ -98,12 +98,13 @@ def lineup_knapsack(players):
             counter['G'] = counter['G'] + 1
             current_lineup_cap += sal 
             continue
-        if counter['F'] < limits['F'] and current_lineup_cap + sal <= cap and pos in ['SF', 'PF','C']:
+        '''if counter['F'] < limits['F'] and current_lineup_cap + sal <= cap and pos in ['SF','PF','SF/PF']:
             lineup.append(player)
             counter['F'] = counter['F'] + 1
             current_lineup_cap += sal 
             continue
-        if counter['UTIL'] < limits['UTIL'] and current_lineup_cap + sal <= cap and pos in ['PG','SG','SF', 'PF','C']:
+        '''
+        if counter['UTIL'] < limits['UTIL'] and current_lineup_cap + sal <= cap and pos in ['PG','SG','SF', 'PF']:
             lineup.append(player)
             counter['UTIL'] = counter['UTIL'] + 1
             current_lineup_cap += sal 
@@ -115,7 +116,7 @@ def lineup_knapsack(players):
         sal = player.salary
         pts = player.points
         if player not in lineup:
-            pos_players = [ x for x in lineup if x.position == pos]
+            pos_players = [x for x in lineup if x.position == pos]
             pos_players.sort(key=lambda x: x.points)
             for pos_player in pos_players:
                 if (current_lineup_cap + sal - pos_player.salary) <= cap and pts > pos_player.points:
